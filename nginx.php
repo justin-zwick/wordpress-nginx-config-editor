5c4c6a3b832ea85f29412c876a1b8a8e48fe6b94
o10n-x <info@optimization.team>
2018-06-14 11:30:32 +0200

    core update

--- nginx.php
+++ nginx.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       Nginx Config Editor
  * Description:       Advanced Nginx config editor for optimization purposes.
- * Version:           0.0.10
+ * Version:           0.0.11
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * GitHub Plugin URI: https://github.com/o10n-x/wordpress-nginx-editor
@@ -30,12 +30,12 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.10';
+$module_version = '0.0.11';
 $minimum_core_version = '0.0.48';
 $plugin_path = dirname(__FILE__);
 
 // load the optimization module loader
-if (!class_exists('\O10n\Module')) {
+if (!class_exists('\O10n\Module', false)) {
     require $plugin_path . '/core/controllers/module.php';
 }
 
